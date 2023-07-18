e2f57fd5c0c0225f6e8fba921ea8de6b9652369a
Travis Gomez <frank.travis.gomez@gmail.com>
2023-07-18 15:47:16 -0500

    adding url into node

--- index.js
+++ index.js
@@ -1,26 +1,40 @@
 const fs = require('fs');
+const http = require('http');
+const url =  require('url')
 
-const textIn = fs.readFileSync('./1-node-farm/starter/txt/input.txt', 'utf-8');
-console.log(textIn);
+//
+// const textIn = fs.readFileSync('./1-node-farm/starter/txt/input.txt', 'utf-8');
+// console.log(textIn);
+//
+// const textOut = `this is what we know about avocados: ${textIn}.\nCreated on ${Date.now()}`;
+// fs.writeFileSync('./1-node-farm/starter/txt/output.txt', textOut);
+// console.log('new file written');
+// // const hello = 'Hello world';
+// // console.log(hello);
+//
+// // non-blocking, asynchronous way
+//
+// fs.readFile('./1-node-farm/starter/txt/start.txt','utf-8',(err, data1)=>{
+//     fs.readFile(`./1-node-farm/starter/txt/${data1}.txt`,'utf-8',(err, data2)=>{
+//         console.log(data2);
+//         fs.readFile('./1-node-farm/starter/txt/append.txt', 'utf-8',(err, data3)=>{
+//             console.log(data3);
+//
+//             fs.writeFile('./1-node-farm/starter/txt/final.txt', `${data2}\n${data3}`, 'utf-8', err=>{
+//                 console.log('new file written');
+//             })
+//         });
+//     });
+// });
+// console.log('read this file');
 
-const textOut = `this is what we know about avocados: ${textIn}.\nCreated on ${Date.now()}`;
-fs.writeFileSync('./1-node-farm/starter/txt/output.txt', textOut);
-console.log('new file written');
-// const hello = 'Hello world';
-// console.log(hello);
+// SERVER
 
-// non-blocking, asynchronous way
-
-fs.readFile('./1-node-farm/starter/txt/start.txt','utf-8',(err, data1)=>{
-    fs.readFile(`./1-node-farm/starter/txt/${data1}.txt`,'utf-8',(err, data2)=>{
-        console.log(data2);
-        fs.readFile('./1-node-farm/starter/txt/append.txt', 'utf-8',(err, data3)=>{
-            console.log(data3);
-
-            fs.writeFile('./1-node-farm/starter/txt/final.txt', `${data2}\n${data3}`, 'utf-8', err=>{
-                console.log('new file written');
-            })
-        });
-    });
+const server = http.createServer((req, res)=>{
+    console.log(req.url)
+    res.end('Hello from the server');
 });
-console.log('read this file');
+
+server.listen(8000, '127.0.0.1', ()=>{
+    console.log('listening to request port 8000')
+});
