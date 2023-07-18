6eff479d32b573bb736bcb9170fe7224abad6c2e
Travis Gomez <frank.travis.gomez@gmail.com>
2023-07-18 10:04:39 -0500

    new node study work

--- index.js
+++ index.js
@@ -14,7 +14,16 @@ console.log('New file');
 
 // non-blocking, asynchronous way
 
-fs.readFile('./1-node-farm/starter/txt/start.txt','utf-8',(err, data)=>{
-    console.log(data);
+fs.readFile('./1-node-farm/starter/txt/start.txt','utf-8',(err, data1)=>{
+    fs.readFile(`./1-node-farm/starter/txt/${data1}.txt`,'utf-8',(err, data2)=>{
+        console.log(data2);
+        fs.readFile('./1-node-farm/starter/txt/append.txt', 'utf-8',(err, data3)=>{
+            console.log(data3);
+
+            fs.writeFile('./1-node-farm/starter/txt/final.txt', `${data2}\n${data3}`, 'utf-8', err=>{
+                console.log('new file written');
+            })
+        });
+    });
 });
 console.log('read this file');
